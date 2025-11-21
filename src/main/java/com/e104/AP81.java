35abbf02827c83a266290e51b4a3255180b08044
jasonKo6612 <jason.ku.tw@gmail.com>
2025-11-21 18:48:31 +0800

    HTHUNTERREQ-2330 獵才 idc AP 轉移 k8s - 改由 /opt/.hunter-env.properties 取得系統參數, gradlew 8.14

--- src/main/java/com/e104/AP81.java
+++ src/main/java/com/e104/AP81.java
@@ -12,14 +12,20 @@ package com.e104;
  */
 
 //import java.io.*;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Properties;
 
-import com.ht.util.XmlGlobalHandlerNewAP;
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.net.SMTPAppender;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.Appender;
+//import com.ht.util.XmlGlobalHandlerNewAP;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 //import com.ht.util.XmlLocalHandlerNewAP;
@@ -33,8 +39,10 @@ import org.slf4j.LoggerFactory;
  */
 public class AP81 {
     private static final Logger logger = LoggerFactory.getLogger(AP81.class);
-
-    private static XmlGlobalHandlerNewAP globalXML = null;
+    private static Properties config = null;
+    private static Connection con = null;
+    private String nodeEnv = "";
+//    private static XmlGlobalHandlerNewAP globalXML = null;
     //private static XmlLocalHandlerNewAP localXML = null;
 
     // Log
@@ -50,7 +58,7 @@ public class AP81 {
      * @throws IOException
      */
     private void queryData() throws IOException {
-        Connection conHun = null;
+//        Connection conHun = null;
         PreparedStatement pstHun = null;
         PreparedStatement pstHun2 = null;
         PreparedStatement pstHun3 = null;
@@ -58,11 +66,11 @@ public class AP81 {
         ResultSet rsHun2 = null;
         try {
             // 建立Connection
-            Class.forName( globalXML.getGlobalTagValue( "dsn1.driver" ) );
-            conHun = DriverManager.getConnection( globalXML.getGlobalTagValue( "dsn1.database" ), globalXML.getGlobalTagValue( "dsn1.username" ), globalXML.getGlobalTagValue( "dsn1.password" ) );
-            pstHun = conHun.prepareStatement( "select contact_id,rid from contact_rec where rid<>'0' and (contact_name='' or contact_name is null) order by Rid" );
-            pstHun2 = conHun.prepareStatement( "Select CName,EName From Resume where Rid=?" );
-            pstHun3 = conHun.prepareStatement( "Update Contact_Rec set contact_name=? where contact_id=?" );
+//            Class.forName( globalXML.getGlobalTagValue( "dsn1.driver" ) );
+//            conHun = DriverManager.getConnection( globalXML.getGlobalTagValue( "dsn1.database" ), globalXML.getGlobalTagValue( "dsn1.username" ), globalXML.getGlobalTagValue( "dsn1.password" ) );
+            pstHun = con.prepareStatement( "select contact_id,rid from contact_rec where rid<>'0' and (contact_name='' or contact_name is null) order by Rid" );
+            pstHun2 = con.prepareStatement( "Select CName,EName From Resume where Rid=?" );
+            pstHun3 = con.prepareStatement( "Update Contact_Rec set contact_name=? where contact_id=?" );
             logger.info("##現有連絡紀錄中 Rid<>0 之人選編號)SQL：select contact_id,rid from contact_rec where rid<>0 and (contact_name='' or contact_name is null) order by Rid");
 
             rsHun = pstHun.executeQuery();
@@ -88,8 +96,8 @@ public class AP81 {
                     }
                 }
             }
-        } catch( ClassNotFoundException e ) {
-            logger.error("queryData() ClassNotFoundException:", e);
+//        } catch( ClassNotFoundException e ) {
+//            logger.error("queryData() ClassNotFoundException:", e);
         } catch( SQLException e ) {
             logger.error("queryData() SQLException:", e);
         } finally {
@@ -128,9 +136,9 @@ public class AP81 {
                     logger.warn("queryData() close pstHun exception:", e);
                 }
             }
-            if( conHun != null ) {
+            if( con != null ) {
                 try {
-                    conHun.close();
+                    con.close();
                 } catch( SQLException e ) {
                     logger.warn("queryData() close connection exception:", e);
                 }
@@ -138,12 +146,101 @@ public class AP81 {
         }
     }
 
+    /**
+     * 設定環境變數與讀取設定檔
+     *
+     * @throws IOException
+     */
+    private void setEnvProperties() throws IOException {
+        nodeEnv = System.getenv("NODE_ENV");
+        if (nodeEnv == null || nodeEnv.isEmpty()) {
+            nodeEnv = "";
+        }
+        logger.info("nodeEnv = {}", nodeEnv);
+
+        String filePath = "/opt/.hunter-env.properties";
+        Properties properties = new Properties();
+        try (FileInputStream fis = new FileInputStream(filePath)) {
+            properties.load(fis);
+            logger.info("成功載入設定檔: {}", filePath);
+        } catch (IOException e) {
+            logger.error("無法讀取設定檔: {}", filePath, e);
+            throw e;
+        }
+        config = properties;
+    }
+
+    /**
+     * 建立資料庫連線
+     *
+     * @param account 屬性鍵值前綴
+     * @return Connection object
+     * @throws SQLException
+     */
+    private Connection createDatabaseConnection(String account) throws SQLException {
+        logger.info("建立資料庫連線: {}", account);
+        String host = config.getProperty(account + ".credential.host");
+        String port = config.getProperty(account + ".credential.port");
+        String database = config.getProperty(account + ".credential.dbClusterIdentifier");
+        String url = String.format("jdbc:mysql://%s:%s/%s?useUnicode=true&characterEncoding=utf8&useSSL=false", host, port, database);
+        logger.info("建立資料庫連線 URL: {}", url);
+        String username = config.getProperty(account + ".credential.username");
+        String password = config.getProperty(account + ".credential.password");
+        logger.info("建立資料庫連線 username: {}", username);
+        logger.info("建立資料庫連線 password: {}", (password != null ? new String(new char[password.length()]).replace("\0", "*") : "null"));
+
+        return DriverManager.getConnection(url, username, password);
+    }
+
+    /**
+     * 設定資料庫連線
+     *
+     * @throws Exception
+     */
+    private void setDataBaseConnection() throws Exception {
+        Class.forName("com.mysql.jdbc.Driver");
+        con = createDatabaseConnection("m008001_b0019_ai");
+    }
+
+    /**
+     * 印出 Logback SMTP Appender 配置資訊
+     */
+    private void printLogbackSmtpConfig() {
+        try {
+            LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+            for (ch.qos.logback.classic.Logger logger : loggerContext.getLoggerList()) {
+                java.util.Iterator<Appender<ILoggingEvent>> iter = logger.iteratorForAppenders();
+                while (iter.hasNext()) {
+                    Appender<ch.qos.logback.classic.spi.ILoggingEvent> appender = iter.next();
+                    if (appender instanceof SMTPAppender) {
+                        SMTPAppender smtpAppender = (SMTPAppender) appender;
+                        logger.info("========== Logback SMTP Appender 配置 ==========");
+                        logger.info("  Appender Name: {}", smtpAppender.getName());
+                        logger.info("  SMTP Host: {}", smtpAppender.getSmtpHost());
+                        logger.info("==============================================");
+                        return; // 找到後就離開
+                    }
+                }
+            }
+        } catch (Exception e) {
+            logger.warn("無法取得 SMTP Appender 配置資訊", e);
+        }
+    }
+
+    public AP81() throws Exception {
+        // 設定環境變數與讀取設定檔
+        setEnvProperties();
+        // 設定資料庫連線
+        setDataBaseConnection();
+        // 印出 Logback SMTP Appender 配置
+        printLogbackSmtpConfig();
+    }
 
     public static void main( String[] args ) throws IOException {
         try {
             // 設定xml
             //localXML = XmlLocalHandlerNewAP.performParser();
-            globalXML = XmlGlobalHandlerNewAP.performParser( 81, "" );
+//            globalXML = XmlGlobalHandlerNewAP.performParser( 81, "" );
             // 設定file
 //            fLogFile = new File( globalXML.getGlobalTagValue( "apini.logpath" ) + "AP81_" + new SimpleDateFormat( "yyyyMMdd" ).format( new Date() ) + ".log" );
 //            fLogFile.createNewFile();
@@ -159,7 +256,8 @@ public class AP81 {
             logger.error("main exception:", e);
         } finally {
             //localXML = null;
-            globalXML = null;
+//            globalXML = null;
+            config = null;
         }
     }
 }
